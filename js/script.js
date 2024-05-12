cab84f09c5cbe5b9059df573a57da479bdd282ed
Ahmed Ehab <a7medehap99@gmail.com>
2024-05-12 21:08:34 +0300

    Fr and It languages added8

--- js/script.js
+++ js/script.js
@@ -101,53 +101,53 @@ $(document).ready(function () {
 // }
 // })
 
-$("#calc").on("click", async function () {
-  console.log();
-  let clacLang = $("#calc").text();
-  let lang = "en";
-  if (clacLang !== "Calculator")
-    lang = "german";
-  let data = 10;
-  await Swal.fire({
-    title: `${lang == "en" ? "Solar Calculator" : "Solarrechner"}`,
-    // text: "Modal with a custom image.",
-    //  imageUrl: "https://unsplash.it/400/200",
+// $("#calc").on("click", async function () {
+//   console.log();
+//   let clacLang = $("#calc").text();
+//   let lang = "en";
+//   if (clacLang !== "Calculator")
+//     lang = "german";
+//   let data = 10;
+//   await Swal.fire({
+//     title: `${lang == "en" ? "Solar Calculator" : "Solarrechner"}`,
+//     // text: "Modal with a custom image.",
+//     //  imageUrl: "https://unsplash.it/400/200",
 
-    // imageWidth: 400,
-    // imageHeight: 200,
-    // imageAlt: "Custom image",
-    input: "number",
+//     // imageWidth: 400,
+//     // imageHeight: 200,
+//     // imageAlt: "Custom image",
+//     input: "number",
 
-    html: `
-    <img src="images/solar1.jpg" width="400px" height="200px" class="d-block m-auto"/>
-   <p class="mt-2">${
-     lang == "en" ? "Enter Space in" : "Geben Sie Leerzeichen ein"
-   } M²</p>
-  `,
-  }).then((result) => {
-    data = result.value;
-  });
-  if (data) {
-    const res = data / 5;
-    let val;
-    if (res >= 1 && res <= 20) val = res * 2500;
-    else if (res > 20 && res <= 50) val = res * 2000;
-    else if (res > 50) val = res * 1700;
-    else val = `${
-      lang == "en"
-        ? "Please enter a valid number"
-        : "Bitte geben Sie eine gültige Nummer ein"
-    }`;
-    Swal.fire(
-      `${
-        isNaN(val)
-          ? val
-          : `${lang == "en" ? "Cost" : "kosten"} = ${new Intl.NumberFormat(
-              "en-US"
-            ).format(val)} CHF.`
-      }`
-    );
-  }
-});
+//     html: `
+//     <img src="images/solar1.jpg" width="400px" height="200px" class="d-block m-auto"/>
+//    <p class="mt-2">${
+//      lang == "en" ? "Enter Space in" : "Geben Sie Leerzeichen ein"
+//    } M²</p>
+//   `,
+//   }).then((result) => {
+//     data = result.value;
+//   });
+//   if (data) {
+//     const res = data / 5;
+//     let val;
+//     if (res >= 1 && res <= 20) val = res * 2500;
+//     else if (res > 20 && res <= 50) val = res * 2000;
+//     else if (res > 50) val = res * 1700;
+//     else val = `${
+//       lang == "en"
+//         ? "Please enter a valid number"
+//         : "Bitte geben Sie eine gültige Nummer ein"
+//     }`;
+//     Swal.fire(
+//       `${
+//         isNaN(val)
+//           ? val
+//           : `${lang == "en" ? "Cost" : "kosten"} = ${new Intl.NumberFormat(
+//               "en-US"
+//             ).format(val)} CHF.`
+//       }`
+//     );
+//   }
+// });
 
 // end calculating
